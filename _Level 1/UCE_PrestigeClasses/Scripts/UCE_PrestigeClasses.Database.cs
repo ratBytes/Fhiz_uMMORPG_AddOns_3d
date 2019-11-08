6572f66d8d232972cdfa45ae7c699b7a1f0ab1d3
Fhiz <40817126+wovencode@users.noreply.github.com>
2019-11-08 14:32:00 +0100

    Update UCE_PrestigeClasses.Database.cs
    - wrapped functions in _SERVER define for improved security on clients

--- _Level 1/UCE_PrestigeClasses/Scripts/UCE_PrestigeClasses.Database.cs
+++ _Level 1/UCE_PrestigeClasses/Scripts/UCE_PrestigeClasses.Database.cs
@@ -6,13 +6,11 @@
 // * Pledge on Patreon for VIP AddOns...: https://www.patreon.com/IndieMMO
 // * Instructions.......................: https://indie-mmo.net/knowledge-base/
 // =======================================================================================
-#if _MYSQL
-using MySql.Data;								// From MySql.Data.dll in Plugins folder
-using MySql.Data.MySqlClient;                   // From MySql.Data.dll in Plugins folder
-#elif _SQLITE
-
-using SQLite; 						// copied from Unity/Mono/lib/mono/2.0 to Plugins
-
+#if _MYSQL && _SERVER
+using MySql.Data;
+using MySql.Data.MySqlClient;
+#elif _SQLITE && _SERVER
+using SQLite;
 #endif
 
 // DATABASE (SQLite / mySQL Hybrid)
@@ -25,13 +23,13 @@ public partial class Database
     [DevExtMethods("Connect")]
     private void Connect_UCE_PrestigeClasses()
     {
-#if _MYSQL
+#if _MYSQL && _SERVER
 		ExecuteReaderMySql(@"CREATE TABLE IF NOT EXISTS character_prestigeclasses (
 			`character` VARCHAR(32) NOT NULL,
 			class1 VARCHAR(32) NOT NULL,
 			class2 VARCHAR(32) NOT NULL
 		) CHARACTER SET=utf8mb4");
-#elif _SQLITE
+#elif _SQLITE && _SERVER
         connection.CreateTable<character_prestigeclasses>();
 #endif
     }
@@ -42,9 +40,9 @@ public partial class Database
     [DevExtMethods("CharacterLoad")]
     private void CharacterLoad_UCE_PrestigeClasses(Player player)
     {
+#if _SERVER
 #if _MYSQL
 		var table = ExecuteReaderMySql("SELECT class1, class2 FROM character_prestigeclasses WHERE `character`=@name", new MySqlParameter("@name", player.name));
-
 #elif _SQLITE
         var table = connection.Query<character_prestigeclasses>("SELECT class1, class2 FROM character_prestigeclasses WHERE character=?", player.name);
 #endif
@@ -58,11 +56,11 @@ public partial class Database
             string class1 = row.class1;
             string class2 = row.class2;
 #endif
-
             UCE_PrestigeClassTemplate prestigeClass1 = null;
             if (UCE_PrestigeClassTemplate.dict.TryGetValue(class1.GetStableHashCode(), out prestigeClass1))
                 player.UCE_prestigeClass = prestigeClass1;
         }
+#endif
     }
 
     // -----------------------------------------------------------------------------------
@@ -71,13 +69,13 @@ public partial class Database
     [DevExtMethods("CharacterSave")]
     private void CharacterSave_UCE_PrestigeClasses(Player player)
     {
-#if _MYSQL
+#if _MYSQL && _SERVER
 		ExecuteNonQueryMySql("DELETE FROM character_prestigeclasses WHERE `character`=@character", new MySqlParameter("@character", player.name));
         ExecuteNonQueryMySql("INSERT INTO character_prestigeclasses VALUES (@character, @class1, @class2)",
 				new MySqlParameter("@character", 	player.name),
 				new MySqlParameter("@class1", 		(player.UCE_prestigeClass != null) ? player.UCE_prestigeClass.name : ""),
 				new MySqlParameter("@class2", 		""));
-#elif _SQLITE
+#elif _SQLITE && _SERVER
         connection.Execute("DELETE FROM character_prestigeclasses WHERE character=?", player.name);
         connection.Insert(new character_prestigeclasses
         {
