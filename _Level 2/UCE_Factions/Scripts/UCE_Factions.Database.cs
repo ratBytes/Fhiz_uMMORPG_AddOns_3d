4746a33bccce52b75a4e4ae8275fe1190b852cec
Fhiz <40817126+wovencode@users.noreply.github.com>
2019-11-08 14:32:16 +0100

    Update UCE_Factions.Database.cs
    - wrapped functions in _SERVER define for improved security on clients

--- _Level 2/UCE_Factions/Scripts/UCE_Factions.Database.cs
+++ _Level 2/UCE_Factions/Scripts/UCE_Factions.Database.cs
@@ -6,15 +6,11 @@
 // * Pledge on Patreon for VIP AddOns...: https://www.patreon.com/IndieMMO
 // * Instructions.......................: https://indie-mmo.net/knowledge-base/
 // =======================================================================================
-using System;
-
-#if _MYSQL
-using MySql.Data;								// From MySql.Data.dll in Plugins folder
-using MySql.Data.MySqlClient;                   // From MySql.Data.dll in Plugins folder
-#elif _SQLITE
-
-using SQLite; 						// copied from Unity/Mono/lib/mono/2.0 to Plugins
-
+#if _MYSQL && _SERVER
+using MySql.Data;
+using MySql.Data.MySqlClient;
+#elif _SQLITE && _SERVER
+using SQLite;
 #endif
 
 // DATABASE (SQLite / mySQL Hybrid)
@@ -27,13 +23,13 @@ public partial class Database
     [DevExtMethods("Connect")]
     private void Connect_UCE_Factions()
     {
-#if _MYSQL
+#if _MYSQL && _SERVER
 		ExecuteNonQueryMySql(@"CREATE TABLE IF NOT EXISTS character_factions (
         				`character` VARCHAR(32) NOT NULL,
         				faction VARCHAR(32)  NOT NULL,
         				rating INTEGER(15)
         				) CHARACTER SET=utf8mb4 ");
-#elif _SQLITE
+#elif _SQLITE && _SERVER
         connection.CreateTable<character_factions>();
 #endif
     }
@@ -44,7 +40,7 @@ public partial class Database
     [DevExtMethods("CharacterLoad")]
     private void CharacterLoad_UCE_Factions(Player player)
     {
-#if _MYSQL
+#if _MYSQL && _SERVER
 		var table = ExecuteReaderMySql("SELECT faction, rating FROM character_factions WHERE `character`=@character", new MySqlParameter("@character", player.name));
 
         foreach (var row in table) {
@@ -53,7 +49,7 @@ public partial class Database
             faction.rating 			= Convert.ToInt32(row[1]);
             player.UCE_Factions.Add(faction);
         }
-#elif _SQLITE
+#elif _SQLITE && _SERVER
         var table = connection.Query<character_factions>("SELECT faction, rating FROM character_factions WHERE character=?", player.name);
 
         foreach (var row in table)
@@ -72,14 +68,14 @@ public partial class Database
     [DevExtMethods("CharacterSave")]
     private void CharacterSave_UCE_Factions(Player player)
     {
-#if _MYSQL
+#if _MYSQL && _SERVER
 		ExecuteNonQueryMySql("DELETE FROM character_factions WHERE `character`=@character", new MySqlParameter("@character", player.name));
         foreach (UCE_Faction faction in player.UCE_Factions)
             ExecuteNonQueryMySql("INSERT INTO character_factions VALUES (@character, @faction, @rating)",
                             new MySqlParameter("@character", player.name),
                             new MySqlParameter("@faction", faction.name),
                             new MySqlParameter("@rating", faction.rating));
-#elif _SQLITE
+#elif _SQLITE && _SERVER
         connection.Execute("DELETE FROM character_factions WHERE character=?", player.name);
         foreach (UCE_Faction faction in player.UCE_Factions)
             connection.Insert(new character_factions
