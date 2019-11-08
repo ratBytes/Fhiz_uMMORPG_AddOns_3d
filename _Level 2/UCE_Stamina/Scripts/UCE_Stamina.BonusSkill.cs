a8a8877f4d15e811fe6d4efab2ee6d28f8cf0dcf
Fhiz <support@wovencode.net>
2019-11-08 17:02:05 +0100

    UCE Stamina AddOn
    - basic AddOn setup

---
+++ _Level 2/UCE_Stamina/Scripts/UCE_Stamina.BonusSkill.cs
@@ -0,0 +1,20 @@
+// =======================================================================================
+// Created and maintained by iMMO
+// Usable for both personal and commercial projects, but no sharing or re-sale
+// * Discord Support Server.............: https://discord.gg/YkMbDHs
+// * Public downloads website...........: https://www.indie-mmo.net
+// * Pledge on Patreon for VIP AddOns...: https://www.patreon.com/IndieMMO
+// * Instructions.......................: https://indie-mmo.net/knowledge-base/
+// =======================================================================================
+using System.Text;
+using UnityEngine;
+using Mirror;
+
+// BONUS SKILL
+
+public abstract partial class BonusSkill : ScriptableSkill
+{
+    public bool blockStaminaRecovery;
+    public LinearInt bonusStaminaMax;
+    public LinearFloat bonusStaminaPercentPerSecond;
+}
