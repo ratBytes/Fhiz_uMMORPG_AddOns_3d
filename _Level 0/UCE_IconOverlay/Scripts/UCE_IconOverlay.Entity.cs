b80b352a0d901e4d309d03bd09537baba4773e8e
Fhiz <support@wovencode.net>
2019-11-09 07:47:43 +0100

    UCE IconOverlay
    - a WIP replacement for Quest overlays and others

---
+++ _Level 0/UCE_IconOverlay/Scripts/UCE_IconOverlay.Entity.cs
@@ -0,0 +1,87 @@
+﻿// =======================================================================================
+// Created and maintained by iMMO
+// Usable for both personal and commercial projects, but no sharing or re-sale
+// * Discord Support Server.............: https://discord.gg/YkMbDHs
+// * Public downloads website...........: https://www.indie-mmo.net
+// * Pledge on Patreon for VIP AddOns...: https://www.patreon.com/IndieMMO
+// =======================================================================================
+using UnityEngine;
+using Mirror;
+using System;
+using System.Linq;
+using System.Collections;
+
+// =======================================================================================
+// ENTITY
+// =======================================================================================
+public partial class Entity {
+
+    public UCE_IconOverlay iconOverlay;
+
+    // iconOverlay.Show(0);
+
+    // -----------------------------------------------------------------------------------
+    // OnClientAggro_UCE_AggroOverlay
+    // -----------------------------------------------------------------------------------
+    /*
+    [ClientCallback]
+    [DevExtMethods("OnClientAggro")]
+    private void OnClientAggro_UCE_IconOverlay(Entity entity)
+    {
+
+    	if (iconOverlay == null || 
+            !isAlive || 
+            !entity.isAlive ||
+            agent.destination == startPosition ||
+            agent.destination == fleePosition ||
+            HealthPercent() < fleeHealthThreshold
+            )
+            return;
+
+        if ( (target == entity || target == null) && entity is Player)
+    		iconOverlay.Show(0);
+    }
+    */
+
+    // -----------------------------------------------------------------------------------
+    // OnDamageDealt_UCE_IconOverlay
+    // -----------------------------------------------------------------------------------
+    /*
+    [ClientCallback]
+    [DevExtMethods("OnDamageDealt")]
+    private void OnDamageDealt_UCE_IconOverlay(int amount)
+    {
+
+        if (iconOverlay == null ||
+            !isAlive ||
+            lastAggressor == null ||
+            (lastAggressor != null && !lastAggressor.isAlive) ||
+            agent.destination == startPosition ||
+            agent.destination == fleePosition ||
+            HealthPercent() < fleeHealthThreshold
+            ) 
+        return;
+
+        if ( (target == lastAggressor || target == null)  && lastAggressor is Player)
+            iconOverlay.Show(0);
+    }
+    */
+
+    // -----------------------------------------------------------------------------------
+    // OnDeath_UCE_IconOverlay
+    // -----------------------------------------------------------------------------------
+    [ClientCallback]
+    [DevExtMethods("OnDeath")]
+    private void OnDeath_UCE_IconOverlay()
+    {
+        if (iconOverlay == null) return;
+
+        iconOverlay.Hide();
+
+    }
+
+    // -----------------------------------------------------------------------------------
+
+}
+
+// =======================================================================================
