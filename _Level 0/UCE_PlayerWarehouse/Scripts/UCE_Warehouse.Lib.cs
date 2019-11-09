fb092183156fec835e0bdb359d4ea46f6b523898
Fhiz <support@wovencode.net>
2019-11-09 07:48:29 +0100

    Update UCE_Warehouse.Lib.cs
    - updated to new defines manager system as well

--- _Level 0/UCE_PlayerWarehouse/Scripts/UCE_Warehouse.Lib.cs
+++ _Level 0/UCE_PlayerWarehouse/Scripts/UCE_Warehouse.Lib.cs
@@ -11,44 +11,16 @@
 using UnityEditor;
 using UnityEngine;
 
-[InitializeOnLoad]
-public class WarehouseLib
+
+public partial class UCE_DefinesManager
 {
-    private const string define = "_iMMOWAREHOUSE";
 
-    static WarehouseLib()
+    [DevExtMethods("Constructor")]
+    public static void Constructor_UCE_PlayerWarehouse()
     {
-        AddLibrayDefineIfNeeded();
+        defines.Add("_iMMOWAREHOUSE");
     }
 
-    private static void AddLibrayDefineIfNeeded()
-    {
-        BuildTargetGroup buildTargetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
-        string definestring = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup);
-        string[] defines = definestring.Split(';');
-
-#if !_iMMOTOOLS
-		Debug.LogWarning("<b>uMMMORPG3d</b> only! I cannot give support for uMMORPG2d or uSurvival - Sorry!");
-
-#endif
-
-        if (Contains(defines, define))
-            return;
-
-        PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroup, (definestring + ";" + define));
-        Debug.LogWarning("<b>AddOn imported!</b> - to complete installation please refer to the included README and follow instructions.");
-        Debug.Log("<b>" + define + "</b> added to <i>Scripting Define Symbols</i> for selected build target (" + EditorUserBuildSettings.activeBuildTarget.ToString() + ").");
-    }
-
-    private static bool Contains(string[] defines, string define)
-    {
-        foreach (string def in defines)
-        {
-            if (def == define)
-                return true;
-        }
-        return false;
-    }
 }
 
 #endif
