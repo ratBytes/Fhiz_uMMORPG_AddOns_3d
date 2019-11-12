af53c63f8c2ed02a6282ba50226b01e5927d8333
Fhiz <support@wovencode.net>
2019-11-12 08:29:20 +0100

    UCE Morale AddOn
    - work in progress

---
+++ _Level 2/UCE_Morale/Scripts/UCE_Morale.Monster.cs
@@ -0,0 +1,36 @@
+﻿// =======================================================================================
+// Created and maintained by iMMO
+// Usable for both personal and commercial projects, but no sharing or re-sale
+// * Discord Support Server.............: https://discord.gg/YkMbDHs
+// * Public downloads website...........: https://www.indie-mmo.net
+// * Pledge on Patreon for VIP AddOns...: https://www.patreon.com/IndieMMO
+// =======================================================================================
+using UnityEngine;
+using Mirror;
+using System;
+using System.Linq;
+using System.Collections;
+
+// =======================================================================================
+// MONSTER
+// =======================================================================================
+public partial class Monster {
+
+    // -----------------------------------------------------------------------------------
+    // OnDamageDealt_UCE_Morale
+    // -----------------------------------------------------------------------------------
+    [DevExtMethods("OnDamageDealt")]
+    private void OnDamageDealt_UCE_Morale(int amount)
+    {
+        morale -= amount;
+    }
+
+
+
+
+
+    // -----------------------------------------------------------------------------------
+
+}
+
+// =======================================================================================
