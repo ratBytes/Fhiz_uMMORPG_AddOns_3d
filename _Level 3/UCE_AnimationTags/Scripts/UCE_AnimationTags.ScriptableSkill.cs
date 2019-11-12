966bf910036e05222844af1303ba4bf03a3efd1a
Fhiz <support@wovencode.net>
2019-11-12 08:28:58 +0100

    UCE Animation Tags AddOn
    - finished

---
+++ _Level 3/UCE_AnimationTags/Scripts/UCE_AnimationTags.ScriptableSkill.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public abstract partial class ScriptableSkill : ScriptableObject
+{
+    [Header("Animation")]
+    public string animationTag;
+}
