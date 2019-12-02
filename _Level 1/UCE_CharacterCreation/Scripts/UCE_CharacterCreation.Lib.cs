29e03562dbdba8925ec97411de863b7ecf1a9cf5
Fhiz <support@wovencode.net>
2019-12-02 19:23:59 +0100

    UCE_CharacterCreation.Lib
    - version increment

--- _Level 1/UCE_CharacterCreation/Scripts/UCE_CharacterCreation.Lib.cs
+++ _Level 1/UCE_CharacterCreation/Scripts/UCE_CharacterCreation.Lib.cs
@@ -24,7 +24,7 @@ public partial class UCE_DefinesManager
         addon.basis         = "uMMORPG3d V1";
         addon.define        = "_iMMOCHARACTERCREATION";
         addon.author        = "Fhiz, Obscured Crow, Pierce";
-        addon.version       = "2019.200";
+        addon.version       = "2019.201";
         addon.dependencies  = "none";
         addon.comments      = "none";
         addon.active        = true;
