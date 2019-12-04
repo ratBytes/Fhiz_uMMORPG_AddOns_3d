27a18fcad4668b43e14f1a5276b97386453dcd63
Fhiz <support@wovencode.net>
2019-12-04 17:50:01 +0100

    Create UCE_SkillLevitate.Lib.cs
    - added lib file

---
+++ _Level 0/UCE_SkillLevitate/Script/UCE_SkillLevitate.Lib.cs
@@ -0,0 +1,36 @@
+// =======================================================================================
+// Created and maintained by iMMO
+// Usable for both personal and commercial projects, but no sharing or re-sale
+// * Discord Support Server.............: https://discord.gg/YkMbDHs
+// * Public downloads website...........: https://www.indie-mmo.net
+// * Pledge on Patreon for VIP AddOns...: https://www.patreon.com/IndieMMO
+// * Instructions.......................: https://indie-mmo.net/knowledge-base/
+// =======================================================================================
+#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEngine;
+
+public partial class UCE_DefinesManager
+{
+
+    [DevExtMethods("Constructor")]
+    public static void Constructor_UCE_SkillLevitate()
+    {
+         UCE_AddOn addon = new UCE_AddOn();
+
+        addon.name          = "UCE Skill Levitate";
+        addon.basis         = "uMMORPG3d V1";
+        addon.define        = "_iMMOSKILLLEVITATE";
+        addon.author        = "Fhiz";
+        addon.version       = "2019.200";
+        addon.dependencies  = "none";
+        addon.comments      = "none";
+        addon.active        = true;
+
+        addons.Add(addon);
+    }
+
+}
+
+#endif
