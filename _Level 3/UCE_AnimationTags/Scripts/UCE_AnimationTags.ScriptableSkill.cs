c6647d62c998c192f4edee01624a957b3c39386d
Ghet <nasse_games@hotmail.com>
2019-12-04 17:25:21 +0100

    Update UCE_AnimationTags.ScriptableSkill.cs

--- _Level 3/UCE_AnimationTags/Scripts/UCE_AnimationTags.ScriptableSkill.cs
+++ _Level 3/UCE_AnimationTags/Scripts/UCE_AnimationTags.ScriptableSkill.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 
-public abstract partial class ScriptableSkill : ScriptableObject
+public abstract partial class ScriptableSkill
 {
     [Header("Animation")]
     public string animationTag;
