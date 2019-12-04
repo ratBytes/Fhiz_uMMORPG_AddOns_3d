9c30bf9ef59f1e232e598370dd5883dc7229f6d7
Fhiz <support@wovencode.net>
2019-12-04 18:09:48 +0100

    Update UCE_Tools.Lib.cs
    - updated lib file

--- _Level 0/_UCE_Tools/Scripts/UCE_Tools.Lib.cs
+++ _Level 0/_UCE_Tools/Scripts/UCE_Tools.Lib.cs
@@ -24,7 +24,7 @@ public partial class UCE_DefinesManager
         addon.basis         = "uMMORPG3d V1";
         addon.define        = "_iMMOTOOLS";
         addon.author        = "Fhiz";
-        addon.version       = "2019.206";
+        addon.version       = "2019.207";
         addon.dependencies  = "none";
         addon.comments      = "none";
         addon.active        = true;
