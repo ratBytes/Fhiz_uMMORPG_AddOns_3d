855a2fa172101b4f5a39c429cfd4fa2689133364
Fhiz <support@wovencode.net>
2019-12-04 17:45:12 +0100

    Create UCE_WeightSystem.Lib.cs
    - added lib file

---
+++ _Level 1/UCE_WeightSystem/Scripts/UCE_WeightSystem.Lib.cs
@@ -0,0 +1,37 @@
+// =======================================================================================
+// Created and maintained by iMMO
+// Usable for both personal and commercial projects, but no sharing or re-sale
+// * Discord Support Server.............: https://discord.gg/YkMbDHs
+// * Public downloads website...........: https://www.indie-mmo.net
+// * Pledge on Patreon for VIP AddOns...: https://www.patreon.com/IndieMMO
+// * Instructions.......................: https://indie-mmo.net/knowledge-base/
+// =======================================================================================
+#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEngine;
+
+public partial class UCE_DefinesManager
+{
+
+    [DevExtMethods("Constructor")]
+    public static void Constructor_UCE_UsageRequirements()
+    {
+       
+        UCE_AddOn addon = new UCE_AddOn();
+
+        addon.name          = "UCE Weight System";
+        addon.basis         = "uMMORPG3d V1";
+        addon.define        = "_iMMOWEIGHTSYSTEM";
+        addon.author        = "Fhiz";
+        addon.version       = "2019.200";
+        addon.dependencies  = "none";
+        addon.comments      = "none";
+        addon.active        = true;
+
+        addons.Add(addon);
+    }
+
+}
+
+#endif
