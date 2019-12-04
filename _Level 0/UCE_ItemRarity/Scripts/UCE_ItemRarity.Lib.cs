26cb9057d4420ae3a88e5c3a6ce034d462ebfac1
Fhiz <support@wovencode.net>
2019-12-04 18:15:03 +0100

    Update UCE_ItemRarity.Lib.cs
    - updated lib file

--- _Level 0/UCE_ItemRarity/Scripts/UCE_ItemRarity.Lib.cs
+++ _Level 0/UCE_ItemRarity/Scripts/UCE_ItemRarity.Lib.cs
@@ -23,7 +23,7 @@ public partial class UCE_DefinesManager
         addon.basis         = "uMMORPG3d V1";
         addon.define        = "_iMMOITEMRARITY";
         addon.author        = "Obscured Crow";
-        addon.version       = "2019.201";
+        addon.version       = "2019.202";
         addon.dependencies  = "none";
         addon.comments      = "none";
         addon.active        = true;
